fd4430e3bfbfa0fc8e41bfaf036d79f5ca456c69
Dominick Brasileiro <dominick.brasileiro1@gmail.com>
2025-02-15 17:30:55 -0300

    add yabai and skhd

---
+++ .dotfiles/skhd/.config/skhd/helpers.sh
@@ -0,0 +1,12 @@
+kill_last_instance() {
+	window_pid=$(yabai -m query --windows --window | jq -r '.pid')
+	count_pid=$(yabai -m query --windows | jq "[.[] | select(.pid == ${window_pid})] | length")
+
+	if [ "$count_pid" -gt 1 ]; then
+		yabai -m window --close
+	else
+		kill "${window_pid}"
+	fi
+}
+
+"$@"
